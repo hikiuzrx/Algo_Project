cbb109916e37d27f55b7226202f639831bdf36e4
idir <idirxdziri@gmail.com>
2024-01-07 12:24:08 +0100

    Inserts a node at the end of the linked list and Inserts a node at a specific position in the linked list

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -16,3 +16,59 @@ Node *insertAtBeginning(Node *head, int data)
     }
     return newNode;
 }
+// Inserts a node at the end of the linked list
+Node *insertAtEnd(Node *head, int data)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    if (head == NULL)
+    {
+        newNode->prev = NULL;
+        return newNode;
+    }
+    Node *current = head;
+    while (current->next != NULL)
+    {
+        current = current->next;
+    }
+    current->next = newNode;
+    newNode->prev = current;
+    return head;
+}
+// Inserts a node at a specific position in the linked list
+Node *insertAtPosition(Node *head, int data, int position)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+
+    if (position == 1)
+    {
+        newNode->next = head;
+        if (head != NULL)
+        {
+            head->prev = newNode;
+        }
+        return newNode;
+    }
+    Node *current = head;
+    for (int i = 1; i < position - 1 && current != NULL; i++)
+    {
+        current = current->next;
+    }
+    if (current == NULL)
+    {
+        return insertAtEnd(head, data);
+    }
+    newNode->next = current->next;
+    if (current->next != NULL)
+    {
+        current->next->prev = newNode;
+    }
+    current->next = newNode;
+    newNode->prev = current;
+
+    return head;
+}
