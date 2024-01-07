1e00ec7bba45ae7ebd5fcf9f63e30d1fe2ab5cee
idir <idirxdziri@gmail.com>
2024-01-07 12:22:30 +0100

    Inserts a node at the beginning of the linked

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -3,3 +3,16 @@
 #include <stdlib.h>
 
 // Function implementations for linked list operations...
+// Inserts a node at the beginning of the linked list
+Node *insertAtBeginning(Node *head, int data)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->prev = NULL;
+    newNode->next = head;
+    if (head != NULL)
+    {
+        head->prev = newNode;
+    }
+    return newNode;
+}
