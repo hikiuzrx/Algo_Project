7780cdb811b344edbb376fd45cde1e693754d88c
Ramzi Wassim Gueracha <121377656+hikiuzrx@users.noreply.github.com>
2024-01-08 21:00:53 +0100

    Delete libraries directory

--- libraries/Animation_functions.h
+++
@@ -1,4 +0,0 @@
-#include "node.h"
-
-void drawNode(Node* current, int index, int numElements);
-void drawList(Node* head, int numElements);
--- libraries/Logic_functions.h
+++
@@ -1,5 +0,0 @@
-
-#include "node.h"
-void Create_Node(Node** h,int d);
-void swapNodes(int* v1, int* v2);
-void Insertion_sort(Node** h);
